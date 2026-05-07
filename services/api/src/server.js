f9f01d3eed0aa089a0f9407e25e01366a37d6363
Mohammad Ismail <153032333+moh0709@users.noreply.github.com>
2026-05-07 23:32:44 +0200

    feat: register planning session routes

--- services/api/src/server.js
+++ services/api/src/server.js
@@ -18,6 +18,7 @@ import { createSourcePathsRouter } from './routes/sourcePaths.routes.js';
 import { createProviderSettingsRouter } from './routes/providerSettings.routes.js';
 import { createAgentBridgeRouter } from './routes/agentBridge.routes.js';
 import { createJobsRouter } from './routes/jobs.routes.js';
+import { createPlanningRouter } from './routes/planning.routes.js';
 
 dotenv.config();
 
@@ -52,6 +53,7 @@ app.use('/api', requireApiToken, createSourcePathsRouter());
 app.use('/api', requireApiToken, createProviderSettingsRouter());
 app.use('/api', requireApiToken, createAgentBridgeRouter());
 app.use('/api', requireApiToken, createJobsRouter());
+app.use('/api', requireApiToken, createPlanningRouter());
 app.use('/api', requireApiToken, createSearchRouter());
 app.use('/api', requireApiToken, createIntelligenceRouter());
 app.use('/api', requireApiToken, createWatchRouter());
