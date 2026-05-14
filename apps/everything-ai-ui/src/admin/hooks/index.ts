88a61870b7236dd767e9e04cda6a88aba1d3adbc
Mohammad Ismail <153032333+moh0709@users.noreply.github.com>
2026-05-14 21:10:21 +0200

    Export admin task runner hook

--- apps/everything-ai-ui/src/admin/hooks/index.ts
+++ apps/everything-ai-ui/src/admin/hooks/index.ts
@@ -1,1 +1,2 @@
 export { default as useAdminLocalSettings, useAdminLocalSettings as namedUseAdminLocalSettings } from './useAdminLocalSettings';
+export { default as useAdminTaskRunner, useAdminTaskRunner as namedUseAdminTaskRunner } from './useAdminTaskRunner';
