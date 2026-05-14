33963a6ea5b117a8f497e67d164cb2a4c5720baa
Mohammad Ismail <153032333+moh0709@users.noreply.github.com>
2026-05-14 21:13:29 +0200

    Export admin provider data hook

--- apps/everything-ai-ui/src/admin/hooks/index.ts
+++ apps/everything-ai-ui/src/admin/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useAdminLocalSettings, useAdminLocalSettings as namedUseAdminLocalSettings } from './useAdminLocalSettings';
+export { default as useAdminProviderData, useAdminProviderData as namedUseAdminProviderData } from './useAdminProviderData';
 export { default as useAdminTaskRunner, useAdminTaskRunner as namedUseAdminTaskRunner } from './useAdminTaskRunner';
